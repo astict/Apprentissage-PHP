62f761cb1e90dda96c2ff012d87e0ba7e2b158bb
astict <thomas.astic31@gmail.com>
2026-09-04 16:30:18 +0200

    Reprendre à 39:31 - N°6

--- N°06-Variables_et_types/index.php
+++ N°06-Variables_et_types/index.php
@@ -1,10 +1,6 @@
 <?php
 
 /*
-    ADRESSE | VALEUR
-    0000      $maVariable = 4
-
-
     4 types scalaires :
         Booléen (boolean, bool)
         Entier (integer, int)
@@ -21,3 +17,17 @@
         Ressources (resource)
         NULL 
 */
+
+$monPrenom = "Thomas";
+echo "Bonjour $monPrenom !\n";
+
+$unBooleen = false;
+$unEntier = 0xAFD7;
+$unFlottant = 3.14;
+$uneChaine = "oui";
+
+$mot = "bien ?";
+echo 'Tu vas '.$mot.' !';   //concaténation
+
+?>
+
