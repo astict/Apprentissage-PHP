8e0beadb9fc4c5c001f01666ae51594632bbbbdf
astict <thomas.astic31@gmail.com>
2026-09-05 10:37:34 +0200

    COMMIT - N°07

---
+++ N°07-Constantes/index.php
@@ -0,0 +1,16 @@
+<?php
+
+define("PSEUDO_ADMIN","Jason");
+define("PI","3.14");
+echo PSEUDO_ADMIN;
+
+echo "<br>";
+
+echo 'Taille max : ' . PHP_INT_MAX . ' | Taille min : ' . PHP_INT_MIN . ' | Taille octets : ' . PHP_INT_SIZE;
+
+echo "<br>";
+
+const NUM = 6.66;
+echo NUM;
+
+?>
